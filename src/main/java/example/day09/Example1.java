d5dce2166a0701b15ac05abc44656209b5bed1d2
KDH <lococo0425@naver.com>
2024-02-23 16:59:03 +0900

    1

--- src/main/java/example/day09/Example1.java
+++ src/main/java/example/day09/Example1.java
@@ -10,14 +10,17 @@ public class Example1 {
         System.out.println("thread.getName() = " + thread.getName());
         // 3. 작업스레드 생성 4가지 방법
             // 자식 익명 객체 : 부모타입 변수명 = new 부모타입(){재정의};
-        Thread threadA = new Thread(){
-            @Override
-            public void run() { // - 작업스레드가 실행할때 최초로 실행되는 함수 재정의
-                Thread thread = Thread.currentThread();
-                System.out.println("작업스레드A : "+thread.getName());
-            }
-        };
-        threadA.start();
+        for( int i = 0 ; i<3 ; i++ ) {  // 작업스레드 3개 생성 // i : main 스레드의 지역변수.
+            Thread threadA = new Thread() {
+                @Override
+                public void run() { // - *1.작업스레드가 실행할때 최초로 실행되는 함수 재정의.
+                    Thread thread = Thread.currentThread();
+                    // * 3. 스레드 이름 변경
+                    thread.setName("내가만든 작업스레드 " ); // i 가 안되는 이유 : main스레드(다른 스레드)의 지역변수 호출 불가능.
+                }
+            };
+            threadA.start();
+        }
 
         // *2. 작업스레드 실행
         System.out.println("3 thread.getName() = " + thread.getName());
